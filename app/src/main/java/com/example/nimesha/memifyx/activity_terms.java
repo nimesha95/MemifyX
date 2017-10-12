f97985d4287284912ecc33757eb8d5716084f1ef
Dineth Kariyawasam <dinethkariyawasam@yahoo.com>
2017-10-12 13:23:41 +0530

    Button Group View

--- app/src/main/java/com/example/nimesha/memifyx/activity_terms.java
+++ app/src/main/java/com/example/nimesha/memifyx/activity_terms.java
@@ -2,6 +2,10 @@ package com.example.nimesha.memifyx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
 
 public class activity_terms extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class activity_terms extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms);
+
+        final CheckBox TermsCheckBox = (CheckBox) findViewById (R.id.termscheckbox);
+        final Button BtnAccept = (Button) findViewById(R.id.btnaccept);
+
+        TermsCheckBox.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //is chkIos checked?
+                if (((CheckBox) v).isChecked()) {
+                    //Case 1
+                    BtnAccept.setClickable(true);
+                    BtnAccept.setEnabled(true);
+                }
+                else {
+                    //case 2
+                    BtnAccept.setClickable(false);
+                    BtnAccept.setEnabled(false);
+                }
+            }
+        });
     }
+
+
+
 }
--- app/src/main/java/com/example/nimesha/memifyx/button_grp.java
+++ app/src/main/java/com/example/nimesha/memifyx/button_grp.java
@@ -1,11 +1,14 @@
 package com.example.nimesha.memifyx;
 
+import android.graphics.Bitmap;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+
+import static com.example.nimesha.memifyx.R.id.image;
 
 public class button_grp extends AppCompatActivity {
 
@@ -14,11 +17,15 @@ public class button_grp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_grp);
 
-        final ImageView imageView1 = (ImageView) findViewById(R.id.imageView10);
-        final ImageView imageView2 = (ImageView) findViewById(R.id.imageView11);
-        final ImageView imageView3 = (ImageView) findViewById(R.id.imageView12);
-        final ImageView imageView4 = (ImageView) findViewById(R.id.imageView13);
-        final ImageView imageView5 = (ImageView) findViewById(R.id.imageView14);
+        final ImageView imageView1 = (ImageView) findViewById(R.id.btnobscene);
+        final ImageView imageView2 = (ImageView) findViewById(R.id.btninsult);
+        final ImageView imageView3 = (ImageView) findViewById(R.id.btnidentityhate);
+        final ImageView imageView4 = (ImageView) findViewById(R.id.btnthreat);
+
+        imageView1.setTag(1);
+        imageView2.setTag(1);
+        imageView3.setTag(1);
+        imageView4.setTag(1);
 
         imageView1.setOnTouchListener(new View.OnTouchListener() {
 
@@ -26,7 +33,22 @@ public class button_grp extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 // TODO Auto-generated method stub
 
-                imageView1.setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
+                if(imageView1.getTag().equals(1)){
+                    imageView1.setImageResource(R.drawable.btn_obscene_new);
+                    imageView1.setTag(2);
+                }
+                else if(imageView1.getTag().equals(2)){
+                    imageView1.setImageResource(R.drawable.btn_obscene_new_1);
+                    imageView1.setTag(3);
+                }
+                else if(imageView1.getTag().equals(3)){
+                    imageView1.setImageResource(R.drawable.btn_obscene_new_2);
+                    imageView1.setTag(4);
+                }
+                else{
+                    imageView1.setImageResource(R.drawable.btn_obscene_new_3);
+                    imageView1.setTag(1);
+                }
                 return false;
             }
         });
@@ -37,7 +59,22 @@ public class button_grp extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 // TODO Auto-generated method stub
 
-                imageView2.setColorFilter(0xFF0000FF, PorterDuff.Mode.MULTIPLY);
+                if(imageView2.getTag().equals(1)){
+                    imageView2.setImageResource(R.drawable.btn_insult_new);
+                    imageView2.setTag(2);
+                }
+                else if(imageView2.getTag().equals(2)){
+                    imageView2.setImageResource(R.drawable.btn_insult_new_1);
+                    imageView2.setTag(3);
+                }
+                else if(imageView2.getTag().equals(3)){
+                    imageView2.setImageResource(R.drawable.btn_insult_new_2);
+                    imageView2.setTag(4);
+                }
+                else{
+                    imageView2.setImageResource(R.drawable.btn_insult_new_3);
+                    imageView2.setTag(1);
+                }
                 return false;
             }
         });
@@ -48,7 +85,22 @@ public class button_grp extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 // TODO Auto-generated method stub
 
-                imageView3.setColorFilter(0xFF777777, PorterDuff.Mode.MULTIPLY);
+                if(imageView3.getTag().equals(1)){
+                    imageView3.setImageResource(R.drawable.btn_identityhate_new);
+                    imageView3.setTag(2);
+                }
+                else if(imageView3.getTag().equals(2)){
+                    imageView3.setImageResource(R.drawable.btn_identityhate_new_1);
+                    imageView3.setTag(3);
+                }
+                else if(imageView3.getTag().equals(3)){
+                    imageView3.setImageResource(R.drawable.btn_identityhate_new_2);
+                    imageView3.setTag(4);
+                }
+                else{
+                    imageView3.setImageResource(R.drawable.btn_identityhate_new_4);
+                    imageView3.setTag(1);
+                }
                 return false;
             }
         });
@@ -59,20 +111,26 @@ public class button_grp extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 // TODO Auto-generated method stub
 
-                imageView4.setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
+                if(imageView4.getTag().equals(1)){
+                    imageView4.setImageResource(R.drawable.btn_threat_new);
+                    imageView4.setTag(2);
+                }
+                else if(imageView4.getTag().equals(2)){
+                    imageView4.setImageResource(R.drawable.btn_threat_new_1);
+                    imageView4.setTag(3);
+                }
+                else if(imageView4.getTag().equals(3)){
+                    imageView4.setImageResource(R.drawable.btn_threat_new_2);
+                    imageView4.setTag(4);
+                }
+                else{
+                    imageView4.setImageResource(R.drawable.btn_threat_new_3);
+                    imageView4.setTag(1);
+                }
                 return false;
             }
         });
-
-        imageView5.setOnTouchListener(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-
-                imageView5.setColorFilter(0xFF000000, PorterDuff.Mode.MULTIPLY);
-                return false;
-            }
-        });
+        
     }
+
 }
