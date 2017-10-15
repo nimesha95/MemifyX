c483023dfaf03a9a4eba35952e4613d3edb9f3c6
nimesha95 <nimesha95@live.com>
2017-10-15 12:19:18 +0530

    Splash Screen Added

--- app/src/main/java/com/example/nimesha/memifyx/SplashActivity.java
+++ app/src/main/java/com/example/nimesha/memifyx/SplashActivity.java
@@ -1,27 +1,63 @@
 package com.example.nimesha.memifyx;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.WindowManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final String TAG = "splash_screen";
     private static int TIME_OUT = 4000; //Time to launch the another activity
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, decision_point.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, TIME_OUT);
+        } else {
+            Log.d(TAG, "onAuthStateChanged:signed_out");
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, TIME_OUT);
+        }
+
+/*
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
                 finish();
             }
         }, TIME_OUT);
+  */
     }
 }
